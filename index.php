cdbb3adbab689aa9b0999de0eb692d8d3863b15b
salvatorebriga <salvo.z2302@gmail.com>
2024-06-21 17:20:26 +0200

    Milestone 2

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'function.php';
+
 $number = intval($_GET['number'] ?? 12);
 
 if ($number < 4) {
@@ -7,26 +9,6 @@ if ($number < 4) {
 }
 
 $password = random_password($number);
-
-function random_password($length)
-{
-    $lower_case = 'abcdefghilmnopqrstuvwxyz';
-    $upper_case = 'ABCDEFGHILMNOPQRSTUVWXYZ';
-    $digits = '1234567890';
-    $symbols = '!@#$%^&*?';
-
-    $all_characters = str_shuffle($lower_case . $upper_case . $digits . $symbols);
-
-    $password = substr(str_shuffle($lower_case), 0, 1);
-    $password .= substr(str_shuffle($upper_case), 0, 1);
-    $password .= substr(str_shuffle($digits), 0, 1);
-    $password .= substr(str_shuffle($symbols), 0, 1);
-
-    $remaining_length = $length - 4;
-    $password .= substr($all_characters, 0, $remaining_length);
-
-    return str_shuffle($password);
-}
 ?>
 
 <!DOCTYPE html>
